5432f3c934700a6b5ed6d9a08741f5e6733b8034
xizzin <xizzincol@gmail.com>
2024-02-06 23:57:40 +0300

    aaa. sleep qaq

---
+++ radius.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <Windows.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+double aaaaa(double a, double b) {
+    double c;
+    c = 3.14 * (a*a - b*b);
+    return c;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите внешний радиус кольца\n";
+    double R;
+    cin >> R;
+    cout << "Введите внутренний радиус кольца\n";
+    double r;
+    cin >> r;
+    double S;
+//оч сильно извиняюсь, проверок нет :(
+    
+    S = aaaaa(R, r);
+    cout << "Результат равен " << S << "\n\n";
+}
+
